689b5db7ee698e3eedc51dc0d932faf16c3c1f80
thanghexp <thanghexpdn1995@gmail.com>
2016-09-19 09:32:55 +0700

    Commit new linux

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function() {
+	return view('home');
+});
+
 Route::get('ThemSanPham', function(){
     $sanpham = new App\SanPham();
     $sanpham->TenSP = "San Pham";
